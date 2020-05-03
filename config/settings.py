e10bb5b660980c65a84c1b31705528c225d8719d
thom03 <thomas.muteti@gmail.com>
2020-05-03 22:09:42 +0300

    Updated docker configuration.

--- config/settings.py
+++ config/settings.py
@@ -20,13 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sq0=i$f1uv3z5v-itzovifao(z5vq6xx0liuzd$+3nj)m(bb9='
+# SECRET_KEY = 'sq0=i$f1uv3z5v-itzovifao(z5vq6xx0liuzd$+3nj)m(bb9='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
